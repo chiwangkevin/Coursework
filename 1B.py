add9c4664951bb40a877075495ed635217706dc1
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-02 23:56:31 +0000

    Update 1B.py

--- 1B.py
+++ 1B.py
@@ -1,2 +1,17 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
+def run():
+  stations=build_station_list()
+  position_of_cam=(52.2053, 0.1218)
+  output = stations_by_distance(stations, p_cam_center)
+   # change each element from (obj, dis) to (name, town, dis)
+    output = [(station.name, station.town, distance) for station, distance in output]
+  
+  
+    print(output[:10])
+    print()
+    print(output[-10:])
+    return [output[:10], output[-10:]]
+if __name__ == "__main__":
+    run()
+Footer
