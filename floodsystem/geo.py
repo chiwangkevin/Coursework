bf5c3ed8e286c3ee7a2c596389c98b1bd248b523
Aditya Tatwawadi <aditya.tatwawadi@gmail.com>
2023-02-05 10:49:04 +0000

    Update geo.py
    more modules

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,6 +9,7 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 from floodsystem.haversine import haversine as hs
 from floodsystem.stationdata import build_station_list
+
 def stations_by_distance(stations, p):
   
     output = []
@@ -22,6 +23,7 @@ def stations_by_distance(stations, p):
     output = sorted_by_key(output, 1)
 
     return output
+  
 def stations_within_radius(stations, center, r):
     
     new_stations = []
@@ -31,3 +33,37 @@ def stations_within_radius(stations, center, r):
             new_stations.append(station)
             
     return new_stations
+  
+ def rivers_with_stations(stations):
+    '''
+    This function returns a set of the name of rivers having at least one station
+    Input :
+        stations:      a list of MonitoringStation objects
+    '''
+    #get the name of rivers with stations
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+        
+    return rivers
+        
+def stations_by_river(stations):
+    '''
+    This function returns a dictionary that map river names to a list of station objects on the river.
+    Input :
+        stations:      a list of MonitoringStation objects
+    '''
+    
+    rivers_with_st = dict()
+    
+    for river in rivers_with_stations(stations):
+        list_of_st = []
+        
+        for station in stations:
+            #forming a list with stations of that river
+            if station.river == river:
+                list_of_st.append(station.name)
+        #adding the list into map
+        rivers_with_st[river] = list_of_st
+        
+    return rivers_with_st
