13d8db46cfcfdb9d4064c8ca61e9505b8a835b7b
Aditya Tatwawadi <aditya.tatwawadi@gmail.com>
2023-02-05 13:18:08 +0000

    Update Task 1E.py
    task 1e change

--- Task 1E.py
+++ Task 1E.py
@@ -1,1 +1,20 @@
+"""
+Created Feb 2023
+@author: at2017
+"""
 
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+def run():
+    '''
+    Test for function: rivers_by_station_number
+    '''
+    
+    stations = build_station_list()
+    output = rivers_by_station_number(stations, 12)
+    print(output)
+    return output
+    
+if __name__ == "__main__":
+    run()
