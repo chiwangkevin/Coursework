1df91cc000ec76b12a1e2a072e0d4326c6a1e75f
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 10:53:19 +0000

    Update Task 1F.py

--- Task 1F.py
+++ Task 1F.py
@@ -1,20 +1,14 @@
 from floodsystem.stationdata import build_station_list
-from floodsystem import station
+from floodsystem.station import *
 
 def run():
-    '''Testing for inconsistent_typical_range_stations
-    '''
-
-    # generate stations list
+    """Requirements for Task 1F"""
     stations = build_station_list()
-
-    # get consistent station names
-    output = station.inconsistent_typical_range_stations(stations)
-
-    print(output)
-    return output
-
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+    inconsistent_stations_name = [i.name for i in inconsistent_stations]
+    inconsistent_stations_name.sort()
+    print(inconsistent_stations_name)
 
 if __name__ == "__main__":
-    a = station.MonitoringStation(0, 0, 0, 0, (1, 1), 0, 0)
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
     run()
