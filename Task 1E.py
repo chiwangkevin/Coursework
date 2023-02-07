edc41a6fa1ae59c0edb782b89476c154b2b4f42e
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 10:52:33 +0000

    Update Task 1E.py

--- Task 1E.py
+++ Task 1E.py
@@ -2,19 +2,15 @@
 Created Feb 2023
 @author: at2017
 """
-
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 
 def run():
-    '''
-    Test for function: rivers_by_station_number
-    '''
-    
+    """Requirements for Task 1E"""
     stations = build_station_list()
-    output = rivers_by_station_number(stations, 12)
-    print(output)
-    return output
-    
+    print(rivers_by_station_number(stations, 9))
+
+
 if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
     run()
