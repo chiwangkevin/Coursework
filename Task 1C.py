c9d72cbdba11c328514548c4007b44f96b63ddd0
Aditya Tatwawadi <aditya.tatwawadi@gmail.com>
2023-02-07 10:53:11 +0000

    Update Task 1C.py

--- Task 1C.py
+++ Task 1C.py
@@ -1,20 +1,15 @@
-from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
-def run():   
+from floodsystem.geo import stations_within_radius
+
+def run():
+    """Requirements for Task 1C"""
+
+    # Build list of stations
     stations = build_station_list()
-    center_cam = (52.2053, 0.1218)
-    r_cam = 10
-    list_stations_within_range = stations_within_radius(stations, center_cam, r_cam)
-    
-    #generate the list of stations within the distance
-    name_list = []
-    for st in list_stations_within_range :
-        name_list.append(st.name)
-    
-    #sorted the list by alphabetic order
-    sorted_list = sorted(name_list)
-    print(sorted_list)
-    return sorted_list
-    
+    stations_within_radius_list = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    alphabetically_sorted = sorted(stations_within_radius_list, key=lambda x: x.name)
+    print([i.name for i in alphabetically_sorted])
+
 if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
     run()
