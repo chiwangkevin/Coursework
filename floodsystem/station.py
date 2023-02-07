3a52e6a1ba94d618b70d7a8ccb61f91f6ab1503d
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 10:54:18 +0000

    Update station.py

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: MIT
 """This module provides a model for a monitoring station, and tools
 for manipulating/modifying station data
-
 """
 
 
@@ -38,43 +37,41 @@ class MonitoringStation:
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
         return d
-        def typical_range_consistent(self):
-        '''
-        Return True if typical_range[0] <= typical_range[1] or typical_range is None
+    
+    def typical_range_consistent(self):
+
+        '''Takes in self (station object)
+        Returns True when typical range is in a correct order
+        Returns False when typical range is in a wrong order/does not exist
         '''
 
-        # check typical_range availability
-        if self.typical_range is None:
+        if self.typical_range == None:
             return False
-        
-        # check consistency
-        return self.typical_range[1] >= self.typical_range[0]
+        elif self.typical_range[0] <= self.typical_range[1]:
+            return True
+        else:
+            return False
 
     def relative_water_level(self):
-        '''Returns relative latest water level
-        if all data are legal
-        '''
-        # verify data concerned
-        if self.latest_level is None or not self.typical_range_consistent():
-            return None
 
-        # store data in the object
-        self.rel_level = (self.latest_level - self.typical_range[0]) / (self.typical_range[1] - self.typical_range[0])
-        
-        # return relavtive water level
-        return self.rel_level
+        '''Takes in self (station object)
+        Returns a value from 0.0 to 1.0 describing the latest relative water level
+        Returns None when data is out of range or inconsistent
+        '''
+
+        if (not self.typical_range_consistent()) or (self.latest_level==None):
+            return None
+        return (self.latest_level-self.typical_range[0])/(self.typical_range[1]-self.typical_range[0])
 
 
 def inconsistent_typical_range_stations(stations):
-    '''This function will apply typical range check to all the input stations
-    It will output station names in alphabetical order whose typical range is inconsistent
+
+    '''Takes in a list of station objects
+    Returns a list of station objects with inconsistent typical range
     '''
 
-    # list comprehension is used here for the reason as follows:
-    # for fun dude~
-    output = [station.name for station in stations if not station.typical_range_consistent()]
-
-    # sort the output in alphabetical order
-    output = sorted(output)
-
-    return output
+    inconsistent_list = []
+    for station in stations:
+        if not station.typical_range_consistent():
+            inconsistent_list.append(station)
+    return inconsistent_list
