a443b32e103aef1d1d06499a933aad7a7e722b1c
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 11:16:54 +0000

    Create Test 1C.py

---
+++ Test 1C.py
@@ -0,0 +1,10 @@
+from Task1C import run
+
+def test_for_1C():
+    '''
+    Test for Task1C by asserting output of Task1C is the same as its output during first running
+    '''
+    in_put = run()
+    assert in_put == ['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool", 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton', 'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
+    
+test_for_1C()
