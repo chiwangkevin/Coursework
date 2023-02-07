38268bbec468a51fd9a7ae64bfb2cea331b6a153
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 16:44:44 +0000

    Update test_geo.py

--- test_geo.py
+++ test_geo.py
@@ -2,9 +2,15 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import *
 
 def test_great_circle_distance():
+    '''
+    test the calculation of haversine function
+    '''
     assert round(great_circle_distance((0.1,0.2),(0.5, 0.6)),1)==62.9
 
-def test_station_by_distance():
+def test_station_by_distance():#
+    '''
+    tests functionality of station_by_distance() function
+    '''
     stations = build_station_list()
     station_by_distance_list = station_by_distance(stations, (52.2053,0.1218))
     #Check it's sorted
@@ -14,6 +20,9 @@ def test_station_by_distance():
         assert i[1]==great_circle_distance(i[0].coord, (52.2053,0.1218))
 
 def test_stations_within_radius():
+    '''
+    tests functionality of test_stations_within_radius()
+    '''
     stations = build_station_list()
     stations_within_radius_list = stations_within_radius(stations, (52.2053,0.1218), 10)
     #Check all are within radius
@@ -21,10 +30,16 @@ def test_stations_within_radius():
         assert great_circle_distance(i.coord, (52.2053,0.1218)) <= 10
 
 def test_rivers_with_station():
+    '''
+    tests functionality of test_rivers_with_station()
+    '''
     stations = build_station_list()
     rivers_with_station_list = rivers_with_station(stations)
 
 def test_stations_by_river():
+    '''
+    Test the functionality of test_stations_by_river()
+    '''
     stations = build_station_list()
     stations_by_river_dict = stations_by_river(stations)
     for i in stations_by_river_dict.keys():
@@ -32,6 +47,9 @@ def test_stations_by_river():
             assert j.river == i
 
 def test_rivers_by_station_number():
+    '''
+    Test the functionality of test_rivers_by_station_number()
+    '''
     stations = build_station_list()
     N = 9
     river_dict = stations_by_river(stations)
