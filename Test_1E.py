a87f5dc8113b2e80b7e90b6ffdfaa96c093f4379
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-07 11:40:13 +0000

    Create Test_1E.py

---
+++ Test_1E.py
@@ -0,0 +1,15 @@
+from Task1E import run
+
+
+def test_for_1E():
+    '''
+    Test for Task1E by asserting output of Task1C is the same as its output during first running
+    '''
+    output = run()
+    standard_output = [('Thames', 55), ('River Avon', 31), ('River Great Ouse', 31), ('River Aire', 21), ('River Calder', 21), ('River Severn', 20), ('River Derwent', 18), ('River Stour', 17), ('River Trent', 15), ('River Wharfe', 15)]
+    print(output)
+    print(standard_output)
+    # assert output == standard_output
+
+
+test_for_1E()
