9df3a4d1c5f8e05ddeded0121979d715b1622cba
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-08 11:30:04 +0000

    Update Task1F.py

--- Task1F.py
+++ Task1F.py
@@ -2,7 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.station import *
 
 def run():
-    """Requirements for Task 1F"""
+    """Requirements for Task 1F, that builds a list of all stations with inconsistent typical range data."""
     stations = build_station_list()
     inconsistent_stations = inconsistent_typical_range_stations(stations)
     inconsistent_stations_name = [i.name for i in inconsistent_stations]
