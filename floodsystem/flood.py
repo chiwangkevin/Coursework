5dede5c61ee1a015d81e730b32668def143aca89
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-19 19:16:19 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,6 @@
 def stations_level_over_threshold(stations, tol):
-  if station.relative_water_level()!=None and station.station.relative_water_level()>tol:
-      output_list=[(i,stationi.relative_water_level()) for i in stations]
-  return output_list
- 
-  
+	'''
+	Takes a list of station objects and a tolerance level.  Returns a list of station objects with relative water level above tolerance.
+	'''
+	output_list = [(station, station.relative_water_level()) for station in stations if station.relative_water_level() != None and station.relative_water_level()>tol]
+	return sorted(output_list, key=lambda x: x[1], reverse=True)
