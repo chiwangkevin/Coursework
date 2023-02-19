5f46f6d55264e08df138985d49daa1fb74885bc3
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-19 19:05:54 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,1 +1,6 @@
-
+def stations_level_over_threshold(stations, tol):
+  if station.relative_water_level()!=None and station.station.relative_water_level()>tol:
+      output_list=[(i,stationi.relative_water_level()) for i in stations]
+  return output_list
+ 
+  
