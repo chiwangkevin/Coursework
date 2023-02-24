6a0df442b44b55a5a3f26afc1cae6961a39a9d5a
chiwangkevin <115924716+chiwangkevin@users.noreply.github.com>
2023-02-24 10:07:08 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -4,5 +4,10 @@ def stations_level_over_threshold(stations, tol):
 	'''
 	output_list = [(station, station.relative_water_level()) for station in stations if station.relative_water_level() != None and station.relative_water_level()>tol]
 	return sorted(output_list, key=lambda x: x[1], reverse=True)
-
+def stations_highest_rel_level(stations, N):
+	'''
+	Takes a list of station objects and integer N.  Returns a list of the N stations (objects) with highest relative water level, sorted in ascending order
+	'''
+	output_list = [(station, station.relative_water_level()) for station in stations if station.relative_water_level() != None]
+	return sorted(output_list, key=lambda x: x[1], reverse=True)[:N]
   
